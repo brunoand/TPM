6a2bbe0ecec11cb94969d1270559ba61dcad2fd3
brunoand <bgabriel.andrade@gmail.com>
2018-10-09 14:55:04 -0300

    newR

--- Scripts/Dada2_QC.R
+++ Scripts/Dada2_QC.R
@@ -11,22 +11,18 @@ args = commandArgs(trailingOnly=TRUE)
 #Input is the path to the Filtered reads (R1 and R2 in the same folder)
 path <- args[1]
 Output <- args[2]
-#LocR1 <- args[3]
-#LocR2 <- args[4]
-#Trim <- args[5]
+
+
+
 
 #These variables will hold the list with the full paths for each R of the paired-end reads
 fnFs <- sort(list.files(path, pattern="_R1_001.(fastq.gz|fastq|fq|fq.gz)", full.names = TRUE))
 fnRs <- sort(list.files(path, pattern="_R2_001.(fastq.gz|fastq|fq|fq.gz)", full.names = TRUE))
 
-write.table(fnFs,"test.txt",sep=";")
-
-#print(fnRs)
-
 #Generating quality plots
-#plot_qualF <- plotQualityProfile(fnFs, aggregate = TRUE)
-#ggsave(paste0(basename(Output), '_R1.pdf'), plot_qualF, device="pdf")
+plot_qualF <- plotQualityProfile(fnFs, aggregate = TRUE)
+ggsave(paste0(basename(Output), '_R1.pdf'), plot_qualF, device="pdf")
 
 
-#plot_qualR <- plotQualityProfile(fnRs, aggregate = TRUE)
-#ggsave(paste0(basename(Output),'_R2.pdf'), plot_qualR, device="pdf")
+plot_qualR <- plotQualityProfile(fnRs, aggregate = TRUE)
+ggsave(paste0(basename(Output),'_R2.pdf'), plot_qualR, device="pdf")
